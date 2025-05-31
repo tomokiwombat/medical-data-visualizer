cb0fb8fb79edf5d268982904ee36d063087b1b1c
Tomoki Kondo <tomoki.kondo1020@gmail.com>
2025-05-31 12:48:28 +0000

    answers

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['BMI'] = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = (df['BMI'] > 25).astype(int)
+df.drop(columns='BMI', inplace=True)
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, 
+                     id_vars=['cardio'], 
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
-
+    cat_plot = sns.catplot(
+        data=df_cat,
+        kind='bar',
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio'
+    )
 
 
     # 8
-    fig = None
+    fig = cat_plot.fig
 
 
     # 9
@@ -38,20 +51,28 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 10))
+
 
     # 15
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 
 
